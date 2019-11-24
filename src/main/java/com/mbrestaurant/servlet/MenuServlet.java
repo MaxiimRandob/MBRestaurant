feccc865b53b11f55944e2ad01f0f3be6702cd3a
MaxiimRandob <madrandob@gmail.com>
2019-11-24 13:01:54 +0200

    404 error statement fixed by mapping servlet in web.xml

--- src/main/java/com/mbrestaurant/servlet/MenuServlet.java
+++ src/main/java/com/mbrestaurant/servlet/MenuServlet.java
@@ -5,16 +5,12 @@ import com.mbrestaurant.service.impl.MenuServiceImpl;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(
-	name = "showMenuServlet",
-	urlPatterns = "/"
-)
+
 public class MenuServlet
 	extends HttpServlet
 {
